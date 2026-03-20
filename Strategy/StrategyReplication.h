cc61d6f6395cfb8e425db5927a9e68b401b60003
Olivia Delonca <o.delonca@gmail.com>
2026-03-20 09:33:11 +0100

    Create StrategyReplication.h

---
+++ Strategy/StrategyReplication.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Option.h"
+#include "BlackScholes.h"
+
+class StrategyReplication {
+public:
+    static double delta(const Option& option);
+    static double cash(const Option& option, double S_current);
+    static double valeurPortefeuille(const Option& option, double S_current);
+};
