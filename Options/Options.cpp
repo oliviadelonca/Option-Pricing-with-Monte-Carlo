d47f1d6383d656670801984410d44f7ee1041abf
Olivia Delonca <o.delonca@gmail.com>
2026-03-20 08:59:58 +0100

    Create Options.cpp

---
+++ Options/Options.cpp
@@ -0,0 +1,33 @@
+#include "Option.h"
+
+Option::Option(double S_, double K_, double T_, double r_, double sigma_, bool isCall_) {
+    S = S_;
+    K = K_;
+    T = T_;
+    r = r_;
+    sigma = sigma_;
+    isCall = isCall_;
+}
+
+Option::Option(const Option& other) {
+    S = other.S;
+    K = other.K;
+    T = other.T;
+    r = other.r;
+    sigma = other.sigma;
+    isCall = other.isCall;
+}
+
+Option& Option::operator=(const Option& other) {
+    if (this != &other) {
+        S = other.S;
+        K = other.K;
+        T = other.T;
+        r = other.r;
+        sigma = other.sigma;
+        isCall = other.isCall;
+    }
+    return *this;
+}
+
+Option::~Option() {}
